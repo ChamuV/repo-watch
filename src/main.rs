91a1a744689a39d2460257bd09060538e5365028
Chamu V <chamuvmg@gmail.com>
2026-01-23 16:16:04 +0530

    First version of authorisation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+// src/main.rs
+use octocrab::Octocrab;
+
+#[tokio::main]
+async fn main() -> octocrab::Result<()> {
+    // Initialising token
+    let token = std::env::var("GITHUB_TOKEN")
+        .expect("GITHUB_TOKEN env is required");
+
+    // Intialising Octocrab for toke
+    let crab = Octocrab::builder()
+        .personal_token(token)
+        .build()?;
+
+    // Authorisation check
+    let me: octocrab::models::UserProfile = crab.get("/user", None::<&()>).await?;
+
+    let login = me.login.to_string();
+    let name = me.name.unwrap_or_else(|| "(no name)".to_string());
+    
+    // Print message for completion
+    println!("Authenticated as: {login} ({name})");
+    Ok(())
 }
